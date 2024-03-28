9b49352ff3fc42b9c05ac129a6c4c95ec12354fd
KISHOREKUMAR2506 <121966667+KISHOREKUMAR2506@users.noreply.github.com>
2024-03-28 19:28:27 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "Who is the highest run-scorer in the IPL histoy?",
+            "Which country won the ICC Cricket World Cup in 2011?",
+            "What is the chemical symbol for gold?",
+            "Who is the current Prime Minister of India?",
+            "Which ocean is the largest in the world?"
+    };
+    public static String choices [][]={
+            {"Sachin Tendulkar" , "Suresh Raina" , "Virat Kohli" , "M S Dhoni"},
+            {"India" , "Australia" , "Sri Lanka" , "England"},
+            {"Au" , "Ag" , "G" , "Go"},
+            {"Narendra Modi" , "Rahul Gandhi" , "Manmohan Singh" , "Donald Trump"},
+            {"Pacific Ocean" , "Atlantic Ocean" , "Indian Ocean" , "Arctic Ocean"}
+    };
+    public static String correctAnswers []={
+            "Virat Kolhi" ,
+            "India",
+            "Au",
+            "Narendra Modi",
+            "Pacific Ocean"};
+}
